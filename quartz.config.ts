0c02c276a51fbd2479c86c6cc11fd0724bd8a6fb
netrebnic <70092629+roblanc@users.noreply.github.com>
2025-02-16 20:25:00 +0200

    yea

--- quartz.config.ts
+++ quartz.config.ts
@@ -16,7 +16,7 @@ const config: QuartzConfig = {
       provider: "plausible",
     },
     locale: "en-US",
-    baseUrl: "quartz.jzhao.xyz",
+    baseUrl: "https://67b22bc20f57c8cea65b55ab--gleaming-stroopwafel-cfd6e8.netlify.app",
     ignorePatterns: ["private", "templates", ".obsidian"],
     defaultDateType: "created",
     generateSocialImages: false,
